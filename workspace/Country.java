044fb9a7fb88c2b66f4b0e141b0e0307bf709e75
DefaultUsernam <121245757+DefaultUsernam@users.noreply.github.com>
2025-01-13 19:04:54 +0000

    commit -m "submission 1/13"

--- workspace/Country.java
+++ workspace/Country.java
@@ -1,12 +1,37 @@
 public class Country
 {
   // add private instance variables for the name, capital, language, and image file.
+  private String cname;
+  private String ctl;
+  private String lang;
+  private String img;
 
   // add constructors
+  public Country(String c,String ct,String l,String i){
+    cname = c;
+    ctl = ct;
+    lang = l;
+    img = i;
+  }
 
   // Write accessor/get methods for each instance variable that returns it.
-
+  public String getCountry(){
+    return cname;
+  }
+  public String getCapital(){
+    return ctl;
+  }
+  public String getLanguage(){
+    return lang;
+  }
+  public String getImage(){
+    return img;
+  }
   // Write a toString() method that returns a concatenated String of 3 of the instance variables in a sentence like "..'s capital is .. and its primary language is ..."
+  public String toString(){
+    String str = cname + "'s capital is " + ctl + " and its primary language is " + lang;
+    return str; 
+  }
  
 
 
--- workspace/Main.java
+++ workspace/Main.java
@@ -11,12 +11,15 @@ public class Main
   private Country[] countryArray = new Country[10];  
   // index of current shown country
   private int index = 0;
+  private int rand;
 
   // GUI elements
   private JFrame jFrame = new JFrame("Countries");
   private ImageIcon img;
   private JLabel imageLabel;
   private JLabel outputLabel;
+  private JTextField input;
+
   
   public static void main(String[] args) {
     // Create the GUI
@@ -40,9 +43,13 @@ public class Main
     // Write a for loop that goes through the countryArray.
     // for(int i ....) {
     // Do the following inside the loop
+    for (int i = 0;i<countryArray.length;i++){
       String input = scan.nextLine();
       String[] data = input.split(",");
       System.out.println("Read in " + data[0]);
+      
+      countryArray[i] = new Country(data[0],data[1],data[2],data[3]);
+      }
       // inside the loop, create a new Country using your constructor with 3 arguments and pass in data[0], data[1], data[2], data[3] as arguments.
      // inside the loop, set countryArray[i] to the created Country object
      
@@ -55,7 +62,7 @@ public class Main
     // Get the country at index from countryArray
     
     // Use its get method to get the its image file name and save it into imagefile variable below instead of worldmap.jpg.
-    String imagefile = "worldmap.jpg";
+    String imagefile = countryArray[index].getImage();
     // Use the following code to create an new Image Icon and put it into the GUI
     img = new ImageIcon("/workspaces/Countries/workspace/"+imagefile);
     imageLabel.setIcon(img);
@@ -64,29 +71,70 @@ public class Main
   /* nextButton should increment index. If the index is greater than 9, reset it back to 0. Clear the outputLabel to empty string using setText, and call showCountry();*/
   public void nextButtonClick()
   {
+    if (index<9)
+    index++;
+    else
+    index = 0;
+    outputLabel.setText("");
+    showCountry();    
+    rand = (int) (Math.random()*3);
+    if (rand == 0){
+    outputLabel.setText("What country is this?");
+    }
     
+    if (rand == 1){
+      outputLabel.setText("What is the country's capital?");
+    }
+
+    if (rand == 2){
+      outputLabel.setText("What langauge is spoken here?");
+    }
   }
   
   /* reviewButton should get the country at index from the countryArray, call its toString() method and save the result, print it out with System.out.println and as an argument to outputLabel.setText( text to print out ); */
   public void reviewButtonClick()
   {
-     
+    String savedresult = countryArray[index].toString();
+    System.out.println(savedresult);
+    outputLabel.setText(savedresult);
   }
 
   /* quizButton should clear the outputLabel (outputLabel.setText to empty string), get the country at index from countryArray, print out a question about it like What country is this? and/or What's this country's capital?. Get the user's answer using scan.nextLine() and check if it is equal to the country's data using its get methods and print out correct or incorrect.
   */
   public void quizButtonClick()
   {
-    Scanner scan = new Scanner(System.in); 
-    
-    
-    
+System.out.println(input.getText());
+    outputLabel.setText("");
+    if (rand == 0){
+    if (input.getText().equals(countryArray[index].getCountry())){
+      outputLabel.setText("Correct!");
+    }
+    else{
+      outputLabel.setText("Incorrect!");
+    }
+  }
+  if (rand == 1){
+    if (input.getText().equals(countryArray[index].getCapital())){
+      outputLabel.setText("Correct!");
+    }
+    else{
+      outputLabel.setText("Incorrect!");
+    }
+  }
+  if (rand == 2){
+    if (input.getText().equals(countryArray[index].getLanguage())){
+      outputLabel.setText("Correct!");
+    }
+    else{
+      outputLabel.setText("Incorrect!");
+    }
+  }
   }
 
 
 
 
-  /* Do NOT change anything below here */
+
   /* The Main() constructor is finished and will construct the GUI */
 public Main() {
     jFrame.setLayout(new FlowLayout());
@@ -108,6 +156,15 @@ public Main() {
         outputLabel = new JLabel();
         jFrame.add(imageLabel);
         jFrame.add(outputLabel);
+
+        //add input
+        input = new JTextField(20);
+        jFrame.add(input);
+
+
+
+
+
         jFrame.setVisible(true);
         // add event listener for button click
         reviewButton.addActionListener(new ActionListener() {
